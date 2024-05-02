c11370652491aa7f8865b633f6938100a3a9687c
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2024-05-02 14:47:10 +0300

    added helper functions to be used instead of repeating

--- Task-01/src/Main.java
+++ Task-01/src/Main.java
@@ -22,42 +22,47 @@ public class Main {
             System.out.print("enter your password: ");
             String password = scanner.nextLine();
 
-            boolean foundInInstructors = false;
-            boolean foundInStudents = false;
-
-            // search in instructors
-            for (Instructor instructor : DB.instructors) {
-                if(username.equals(instructor.username) && password.equals(instructor.password)) {
-                    foundInInstructors = true;
-                    break;
-                }
-            }
-            // search in students
-            for (Student student : DB.students) {
-                if(username.equals(student.username) && password.equals(student.password)) {
-                    foundInStudents = true;
-                    break;
-                }
-            }
+            boolean foundInInstructors = IsFoundIn(
+                    DB.instructors, username, password
+            );
+            boolean foundInStudents =  foundInInstructors ? false : IsFoundIn(DB.students, username, password);
+//            boolean foundInStudents = !foundInInstructors && IsFoundIn(DB.students, username, password); SMARTER SOLUTION
 
             if(foundInStudents) {
-                // print all courses
                 System.out.println("Login successful as Student");
-                for (Course course : DB.courses) {
-                    System.out.println("- " + course.title);
-                }
+                printCourses(DB.courses);
                 appStillRunning = false; // to finish execution
+
             } else if (foundInInstructors) {
                 System.out.println("Login successful as Instructor");
-                // print all students
-                for (Student student : DB.students) {
-                    System.out.println("- " + student.username);
-                }
+                printUsers(DB.students);
                 appStillRunning = false;// to finish execution
             } else {
                 System.out.println("wrong username or password, try again!!");
             }
 
         } while(appStillRunning);
+    }
+
+    // helper function to search for Instructor/Student/AnyClassExtendsUser
+    static boolean IsFoundIn(User[] users, String username, String password) {
+        for (User user : users) {
+            if(username.equals(user.username) && password.equals(user.password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static void printCourses(Course[] courses) {
+        for (Course course : courses) {
+            System.out.println("- " + course.title);
+        }
+    }
+
+    static void printUsers(User[] users) {
+        for (User user : users) {
+            System.out.println("- " + user.username);
+        }
     }
 }
